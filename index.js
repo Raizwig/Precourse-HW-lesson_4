1ed3d49fa6b1d767145f0119db29555f34a5e869
Raizwig <105453771+Raizwig@users.noreply.github.com>
2024-04-20 06:57:26 +0300

    Первая проверка

--- index.js
+++ index.js
@@ -2,22 +2,27 @@ let timerId; // переменная, которая будет хранить I
 
 const startButton = document.getElementById("start");
 startButton.addEventListener("click", function () {
+  stopClock();
   // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ setInterval
   timerId = setInterval(updateClock, 1000); // запускаем  updateClock() каждую секунду
 });
 
-const stopButton = ДОКУМЕНТ.ГЕТэЛЕМЕНТбАЙаЙДИ(КАКОЙ - ТО);
+const stopButton = document.getElementById("stop");
 stopButton.addEventListener("click", function () {
   // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ clearInterval
   clearInterval(timerId); // останавливаем таймер
 });
 
+function stopClock() {
+  clearInterval(timerId);
+}
+
 function updateClock() {
   const clock = document.getElementById("clock");
   // НУЖНО ПОГУГЛИТЬ ЧТО ТАКОЕ new Date()
-  const now = new Date(2024, 3, 16, 22, 04, 59);
-  const hours = now.ДАЙчАСЫ();
-  const minutes = now.ДАЙмИНУТЫ();
-  const seconds = now.ДАЙсЕКУНДЫ();
-  clock.textContent = hours + ":" + minutes + ":" + seconds;
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  clock.textContent = hours + " : " + minutes + " : " + seconds;
 }
